ec19adac8eca36e9379bfc7f67aa82b9bc5a27db
Vibha Kamath <102431826+vibhakamath23@users.noreply.github.com>
2025-12-03 14:24:45 -0500

    Update HapticPinArrayBottomSide.ino

--- HapticPinArrayBottomSide.ino
+++ HapticPinArrayBottomSide.ino
@@ -1,45 +1,41 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <Servo.h>
 
 // Create Servo Driver for the 16-channel shield
-Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
+Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
 
 #define SERVO_MIN 150
 #define SERVO_MAX 600
 
 // ANGLES FOR THE SERVOS 
-int motorAngles2[5][4] = {
-  {10, 20, 30, 40},        // [0][0] skipped
-  {50, 60, 70, 80},        // [1][0] skipped
-  {90, 100, 110, 120},     // [2][0] skipped
-  {130, 140, 150, 160},    // [3][0] skipped
-  {170, 180, 190, 200}     // full row used
+int n = 0;
+int motorAngles[3][7] = {
+  {n, n, n, n, n, n, n},   
+  {n, n, n, n, n, n, n},   
+  {n, n, n, n, n, n, n},    
 };
 
 int angleToPulse(int angle) {
   return map(angle, 0, 180, SERVO_MIN, SERVO_MAX);
 }
 
-void setShield2Servos(int array[][4], Adafruit_PWMServoDriver &driver) {
+void setShieldServos(int array[][7], int rows, int cols, Adafruit_PWMServoDriver &driver) {
 
   int channel = 0;
 
-  for (int row = 0; row < 5; row++) {
-    for (int col = 0; col < 4; col++) {
+  for (int row = 0; row < rows; row++) {
 
-      // Skip [0..3][0] - covered by other arduino
-      if (row < 4 && col == 0) {
-        continue;
-      }
+    // If we are on row 0, skip the first 5 motors → start at col = 5
+    int colStart = (row == 0) ? 5 : 0;
 
-      if (channel >= 16) {
-        return; // shield is full
-      }
+    for (int col = colStart; col < cols; col++) {
 
-      int angle = array[row][col];
+      if (channel >= 16)
+        return;
 
-      driver.setPWM(channel, 0, angleToPulse(angle));
-      delay(40);
+      driver.setPWM(channel, 0, angleToPulse(array[row][col]));
+      delay(300);
 
       channel++;
     }
@@ -48,14 +44,13 @@ void setShield2Servos(int array[][4], Adafruit_PWMServoDriver &driver) {
 
 void setup() {
   Wire.begin();
-  pwm2.begin();
-  pwm2.setPWMFreq(50);
+  pwm1.begin();
+  pwm1.setPWMFreq(50);
 }
 
 void loop() {
 
-  // Send processed 16 values to shield #2
-  setShield2Servos(motorAngles2, pwm2);
+  setShieldServos(motorAngles, 3, 7, pwm1);
 
-  delay(800);
+  delay(1000);
 }
