2e397dba85e14207db93bb899499d22bfbc9204a
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2025-12-05 14:28:41 -0500

    Refactor servo angle selection with FLAG_NAME

--- HapticPinArrayBottomSide.ino
+++ HapticPinArrayBottomSide.ino
@@ -8,12 +8,20 @@ Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
 #define SERVO_MIN 150
 #define SERVO_MAX 600
 
+#define ZERO 0
+#define JAP 1
+#define DEN 2
+#define SWE 3
+//Choose
+#define FLAG_NAME 1
+
+
 // ANGLES FOR THE SERVOS 
-int n = 45; // GO TO 160 !!!! max 
+int n = 0; // GO TO 160 !!!! max 
 int motorAnglesZero[3][7] = {
   {n, n, n, n, n, n, n},   
   {n, n, n, n, n, n, n},   
-  {n, n, n, n, n, n, n},     // 0 40 65 99 highest to lowest
+  {n, n, n, n, n, n, n},
 };
 
 int white = 99;
@@ -25,33 +33,30 @@ int red = 40;
 int motorAnglesJapan[3][7] = {
   {white, white, red, red, red, white, white},   
   {white, white, white, red - 10, white, white, white},   
-  {white, white, white, white, white, white, white},     // 0 40 65 99 highest to lowest
+  {white, white, white, white, white, white, white},
 };
 
 int motorAnglesDenmark[3][7] = {
   {white, white, white, white, white, white, white},   
   {red + 10, red, white, red, red - 10, red, red - 10},   
-  {red - 20, red - 10, white, red, red, red, red},       // 0 40 65 99 highest to lowest
+  {red - 20, red - 10, white, red, red, red, red},
 };
 
 int motorAnglesSweden[3][7] = {
   {yellow, yellow, yellow, yellow, yellow, yellow, yellow},   
   {blue, blue, yellow, blue, blue, blue, blue},   
-  {blue, blue, yellow, blue, blue, blue, blue},       // 0 40 65 99 highest to lowest
+  {blue, blue, yellow, blue, blue, blue, blue},
 }; 
-
 
 int angleToPulse(int angle) {
   return map(angle, 0, 180, SERVO_MIN, SERVO_MAX);
 }
 
 void setShieldServos(int array[][7], int rows, int cols, Adafruit_PWMServoDriver &driver) {
-
   int channel = 0;
 
   for (int row = 0; row < rows; row++) {
 
-    // If we are on row 0, skip the first 5 motors → start at col = 5
     int colStart = (row == 0) ? 5 : 0;
 
     for (int col = colStart; col < cols; col++) {
@@ -60,7 +65,7 @@ void setShieldServos(int array[][7], int rows, int cols, Adafruit_PWMServoDriver
         return;
 
       driver.setPWM(channel, 0, angleToPulse(array[row][col]));
-      delay(300);
+      delay(400);
 
       channel++;
     }
@@ -75,7 +80,30 @@ void setup() {
 
 void loop() {
 
-  setShieldServos(motorAnglesZero, 3, 7, pwm1);
+  // Pointer to the matrix we want to use
+  int (*selectedMatrix)[7];
+
+  // Pick based on FLAG_NAME
+  switch (FLAG_NAME) {
+    case ZERO:
+      selectedMatrix = motorAnglesZero;
+      break;
+    case JAP:
+      selectedMatrix = motorAnglesJapan;
+      break;
+    case DEN:
+      selectedMatrix = motorAnglesDenmark;
+      break;
+    case SWE:
+      selectedMatrix = motorAnglesSweden;
+      break;
+    default:
+      selectedMatrix = motorAnglesZero; // fallback
+  }
+
+  // Use selected matrix
+  setShieldServos(selectedMatrix, 3, 7, pwm1);
 
   delay(1000);
 }
+
